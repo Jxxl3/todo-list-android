10059f3b82059b202cec2d302cbe5f0c973544e5
Jxxl3 <SmithJJ3@hendrix.edu>
2024-09-11 04:01:10 -0500

    Updated to_do_items.dart
    Before the name and the abbreviation was switched but now the text is where its supposed to be and the abbreviation is now where the avatar is.

--- to_dont_list/lib/widgets/to_do_items.dart
+++ to_dont_list/lib/widgets/to_do_items.dart
@@ -5,33 +5,24 @@ typedef ToDoListChangedCallback = Function(Item item, bool completed);
 typedef ToDoListRemovedCallback = Function(Item item);
 
 class ToDoListItem extends StatelessWidget {
-  ToDoListItem(
-      {required this.item,
-      required this.completed,
-      required this.onListChanged,
-      required this.onDeleteItem})
-      : super(key: ObjectKey(item));
+  ToDoListItem({
+    required this.item,
+    required this.completed,
+    required this.onListChanged,
+    required this.onDeleteItem,
+  }) : super(key: ObjectKey(item));
 
   final Item item;
   final bool completed;
-
   final ToDoListChangedCallback onListChanged;
   final ToDoListRemovedCallback onDeleteItem;
 
   Color _getColor(BuildContext context) {
-    // The theme depends on the BuildContext because different
-    // parts of the tree can have different themes.
-    // The BuildContext indicates where the build is
-    // taking place and therefore which theme to use.
-
-    return completed //
-        ? Colors.black
-        : Theme.of(context).primaryColor;
+    return completed ? Colors.black54 : Theme.of(context).primaryColor;
   }
 
   TextStyle? _getTextStyle(BuildContext context) {
     if (!completed) return null;
-
     return const TextStyle(
       color: Colors.black54,
       decoration: TextDecoration.lineThrough,
@@ -44,17 +35,13 @@ class ToDoListItem extends StatelessWidget {
       onTap: () {
         onListChanged(item, completed);
       },
-      onLongPress: completed
-          ? () {
-              onDeleteItem(item);
-            }
-          : null,
+      onLongPress: completed ? () => onDeleteItem(item) : null,
       leading: CircleAvatar(
         backgroundColor: _getColor(context),
-        child: Text(item.name),
+        child: Text(item.abbrev()),
       ),
       title: Text(
-        item.abbrev(),
+        item.name, // Use the full item name here
         style: _getTextStyle(context),
       ),
     );
